02347a517609e6a202af84f9e79a5d83b275cea3
sergio <sergiocanalesm1@gmail.com>
2021-03-05 01:26:06 -0500

    Initial Commit

---
+++ iOSlab/iOSlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSlabApp.swift
+//  iOSlab
+//
+//  Created by 1234 on 5/03/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
