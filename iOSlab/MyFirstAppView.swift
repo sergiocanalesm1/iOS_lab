21540c60204d1f82bd64bfc6033f6438fe27766a
sergio <sergiocanalesm1@gmail.com>
2021-03-05 09:34:35 -0500

    final view added

--- iOSlab/MyFirstAppView.swift
+++ iOSlab/MyFirstAppView.swift
@@ -7,14 +7,35 @@
 
 import SwiftUI
 
+
 struct MyFirstAppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct MyFirstAppView_Previews: PreviewProvider {
-    static var previews: some View {
-        MyFirstAppView()
+        VStack {
+            HStack {
+                Text("My First App")
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            .padding()
+            .background(Color("primary"))
+            Spacer()
+            Image("labGirl")
+                .resizable()
+                .frame(width: 350, height: 200)
+                .padding(.top, 50)
+            Text( """
+                "The more I deal with the work as something that is my own, as something that is personal, the more successful it is." --
+                Marian Bantjes, designes and author
+                """)
+                .fontWeight(.light)
+                .padding(35)
+                .multilineTextAlignment(.center)
+                    Text("LOG OUT!")
+                        .frame(minWidth: 0, maxWidth: 100, minHeight: 0,maxHeight: 5)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color("primary"))
+        }
     }
 }
